0af85705546747ff92820b35a8933569160ad54d
byun3491byun <byun3491@gmail.com>
2019-02-08 09:37:51 +0900

    팹파일 체인지

--- fabfile.py
+++ fabfile.py
@@ -3,30 +3,23 @@ from fabric.api import local, run, sudo, env
 import os
 import json
 
+PROJECT_DIR = os.path.dirname( os.path.abspath(__file__) )
+envs = json.load( open( os.path.join(PROJECT_DIR,'deploy.json') ) )
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-envs = json.load( open( os.path.join( PROJECT_DIR, 'deploy.json' ) ) )
-
-
-
-REPO_URL = envs['REPO_URL']
-PROJECT_NAME = envs['PROJECT_NAME']
-REMOTE_HOST = envs['REMOTE_HOST']
+REPO_URL        = envs['REPO_URL']
+PROJECT_NAME    = envs['PROJECT_NAME']
+REMOTE_HOST     = envs['REMOTE_HOST']
 REMOTE_HOST_SSH = envs['REMOTE_HOST_SSH']
-REMOTE_USER = envs['REMOTE_USER']
+REMOTE_USER     = envs['REMOTE_USER']
 
-
-env.user = REMOTE_USER
+env.user  = REMOTE_USER
 env.hosts = [
     REMOTE_HOST_SSH,
 ]
-
 env.use_ssh_config = True
-env.key_filename = '../byunkey.pem'
-project_folder = '/home/{}/{}'.format(env.user, PROJECT_NAME)
-print(project_folder)
+env.key_filename   = 'byunkey.pem'
 
+project_folder = '/home/{}/{}'.format(env.user,PROJECT_NAME)
 
 apt_requirements = [
     'curl',
@@ -41,7 +34,6 @@ apt_requirements = [
     'libffi-dev'
 ]
 
-
 def new_initServer():
     _setup()
     update()
@@ -50,34 +42,31 @@ def _setup():
     _init_apt()
     _install_apt_packages( apt_requirements )
     _making_virtualenv()
-    
 
 def _init_apt():
     yn = input('ubuntu linux update ok?: [y/n]')
     if yn == 'y':
         sudo('apt-get update && apt-get -y upgrade')
 
-
-def _install_apt_packages( requires ):
+def _install_apt_packages( requires ):    
     reqs = ''
-    for req in requires:
-        reqs += ' ' + req 
-    sudo( 'apt-get -y install' +reqs )
+    for req in requires:        
+        reqs += ' ' + req    
+    sudo( 'apt-get -y install ' + reqs )
 
-def _making_virtualenv():
+def _making_virtualenv():    
     if not exists('~/.virtualenvs'):
-        run('mkdir ~/.virtualenvs')
-        sudo('pip3 install virtualenv virtualenvwrapper')
+        run('mkdir ~/.virtualenvs')        
+        sudo('pip3 install virtualenv virtualenvwrapper')        
         cmd = '''
             "# python virtualenv global setting
             export WORKON_HOME=~/.virtualenvs
-            #python location
-            export VIRTUALENVWRAPPER_PYTHON="$(commend \which python3)"
-            #쉘 실행
+            # python location
+            export VIRTUALENVWRAPPER_PYTHON="$(command \which python3)"
+            # shell 실행
             source /usr/local/bin/virtualenvwrapper.sh"
         '''
-        run('echo {} >> ~/.bashrc'.format(cmd))
-
+        run('echo {} >> ~/.bashrc'.format(cmd) ) 
 
 def update():
     _git_update()
@@ -85,23 +74,24 @@ def update():
     _virtualhost_make()
     _grant_apache()
     _restart_apache()
- 
+
 def _git_update():
-    if exists(project_folder + '/.git'): 
+    if exists(project_folder + '/.git'):
         run('cd %s && git fetch' % (project_folder,))
-    else: 
-        run('git clone %s %s' % (REPO_URL, project_folder))
+    else:        
+        run('git clone %s %s' % (REPO_URL, project_folder))    
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run('cd %s && git reset --hard %s' % (project_folder, current_commit))
-
+    
 def _virtualenv_update():
     virtualenv_folder = project_folder + '/../.virtualenvs/{}'.format(PROJECT_NAME)
+    
     if not exists(virtualenv_folder + '/bin/pip'):
         run('cd /home/%s/.virtualenvs && virtualenv %s' % (env.user, PROJECT_NAME))
+
     run('%s/bin/pip install -r %s/requirements.txt' % (
         virtualenv_folder, project_folder
     ))
-
 
 def _ufw_allow():
     sudo("ufw allow 'Apache Full'")
@@ -134,8 +124,6 @@ def _virtualhost_make():
 def _grant_apache():
     sudo('chown -R :www-data ~/{}'.format(PROJECT_NAME))
     sudo('chmod -R 775 ~/{}'.format(PROJECT_NAME))
-
-
 
 def _restart_apache():
     sudo('service apache2 restart')
